8e5fec1ca04e0e2b0f050e0c3968e0d8ab940ef2
gleysii <gleysibianchi@gmail.com>
2023-01-28 19:49:32 -0300

    galeria modal em grid

--- js/script-modal.js
+++ js/script-modal.js
@@ -1,12 +1,16 @@
+
+//seleções
 let imagens = document.querySelectorAll('.small_img');
 let modal = document.querySelector('.modal');
 let modalImg = document.querySelector('#modal_img');
 let btClose = document.querySelector('#bt_close');
 let srcVal = "";
 
+//percorrendo em looping
 for(let i = 0; i < imagens.length; i++){
+    //criação de função anônima
     imagens[i].addEventListener('click', function(){
-        srcVal = imagens[i].getAttribute('src');
+        srcVal = imagens[i].getAttribute('src');//o atributo é o endereço
         modalImg.setAttribute('src', srcVal);
         modal.classList.toggle('modal_active');
     });
